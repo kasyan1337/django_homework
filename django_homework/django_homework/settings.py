8b2a3b19b371ac6cf1f6475fd671fd3d3fead804
kasyan1337 <tristokorun@gmail.com>
2024-05-12 01:23:03 +0800

    Step 2 - nastroiki + new project

--- django_homework/django_homework/settings.py
+++ django_homework/django_homework/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5)6&pxymh**5q$(bk4pwash_4g#r0@j)bz-omd@3y!w29q7-ur
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -116,6 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (BASE_DIR / 'static',)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
