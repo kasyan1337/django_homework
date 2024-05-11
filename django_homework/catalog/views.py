e2ad94f5af4b0771bf0916f2635305f101168422
kasyan1337 <tristokorun@gmail.com>
2024-05-12 02:27:56 +0800

    Homework 2 - Bonus task done without success message and redirect

--- django_homework/catalog/views.py
+++ django_homework/catalog/views.py
@@ -1,5 +1,6 @@
 # Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 
 def home(request):
@@ -7,4 +8,16 @@ def home(request):
 
 
 def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+
+        # Do something with the data
+        print(f"Received contact info - Name: {name}, Phone: {phone}, Message: {message}")
+        # messages.success(request, 'Thank you for your message!') # Added a success message for the user
+        # commented, would have to edit the html to make it work
+
+        # return redirect('contact')  # Redirect to the same page or to a success page
+
     return render(request, 'catalog/contact.html')
