bde4875dec1fced8bbe688a838dbe407fefe15e7
kasyan1337 <tristokorun@gmail.com>
2024-05-13 09:24:47 +0800

    Homework 3 -
    manufactured_at removed from models.py

--- django_homework/catalog/models.py
+++ django_homework/catalog/models.py
@@ -23,7 +23,6 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    manufactured_at = models.DateField(null=True, blank=True)
 
     class Meta:
         verbose_name = "Product"
