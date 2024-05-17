f33f40f0fbb2aafce62a6f6c8caf99911f43284a
kasyan1337 <tristokorun@gmail.com>
2024-05-18 04:30:47 +0800

    Homework 4 - Done

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,21 +3,22 @@ Added manually
 Here is a copypaste from the urls.py file from the other directory:
 Admin doesn't have to be here.
 """
-# from django.contrib import admin
-from django.urls import path, include
-from .apps import CatalogConfig
-from . import views
-
 from django.conf import settings
 from django.conf.urls.static import static
+# from django.contrib import admin
+from django.urls import path
+
+from . import views
+from .apps import CatalogConfig
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('contacts/', views.contact, name='contacts'), # Changed to 'contacts/' because in the contact.html file
-    # the form action is set to 'contacts/'
+    path('contacts/', views.contact, name='contacts'),
+    # Not needed anymore because I added the product_list to the home page
+    # path('product_list/', views.product_list, name='product_list'),
+    path('product_page_details/<int:pk>/', views.product_detail, name='product_page_details'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,10 @@
 # Create your views here.
-from django.shortcuts import render, redirect
-from django.contrib import messages
+from .models import Product
 
 
 def home(request):
-    return render(request, 'catalog/home.html')
+    products = Product.objects.all()  # Fetch all products, otherwise it will be empty
+    return render(request, 'catalog/home.html', {'products': products})
 
 
 def contact(request):
@@ -15,9 +15,25 @@ def contact(request):
 
         # Do something with the data
         print(f"Received contact info - Name: {name}, Phone: {phone}, Message: {message}")
-        # messages.success(request, 'Thank you for your message!') # Added a success message for the user
-        # commented, would have to edit the html to make it work
-
-        # return redirect('contact')  # Redirect to the same page or to a success page
 
     return render(request, 'catalog/contact.html')
+
+
+# Not needed anymore because I added the product_list to the home page
+# def product_list(request, pk=1):
+#     products = Product.objects.all()
+#     context = {
+#         'products': products
+#     }
+#     return render(request, 'catalog/product_list.html', context)
+
+
+from django.shortcuts import render, get_object_or_404
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {
+        'product': product
+    }
+    return render(request, 'catalog/product_detail.html', context)
